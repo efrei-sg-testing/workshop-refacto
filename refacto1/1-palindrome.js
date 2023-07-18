c87d45d99823c1dc38aa0951139ed64c0e0c0210
Adil Sabir <m.sabir.adil@gmail.com>
2023-07-18 15:07:07 +0100

    exo 1 - palindrome

--- refacto1/1-palindrome.js
+++ refacto1/1-palindrome.js
@@ -1,4 +1,17 @@
 // Détermine si le paramètre passé est un palindrome
 // ex: "node 1-palindrome.js aha"
 
-console.log("Ceci " + (process.argv[2].split("").reverse('').join("") === process.argv[2] ? "est" : "n'est pas") + " un palindrome")
+//console.log("Ceci " + (process.argv[2].split("").reverse('').join("") === process.argv[2] ? "est" : "n'est pas") + " un palindrome")
+
+
+
+function checkPalindrome(wordToCheck) {
+    const reversed = wordToCheck.split("").reverse().join("");
+    const isPalindrome = (reversed === wordToCheck);
+    return isPalindrome;
+}
+
+const commandeLineArgument = process.argv[2]
+const isWordPalindrome = checkPalindrome(commandeLineArgument)
+
+isWordPalindrome ? console.log("Ceci est un plaindrome") : console.log("ceci n'est pas un plaindrome")
